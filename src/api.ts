a0a792810102cd479486249cc05a3bb76437e4a2
byungsker <extreme93@naver.com>
2023-10-06 00:14:51 +0900

    9.7 Home Screen part Two

--- src/api.ts
+++ src/api.ts
@@ -8,6 +8,24 @@ const BASE_PATH = "https://api.themoviedb.org/3";
 //     (response) => response.json()
 //   );
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   const options = {
     method: "GET",
